d48438d2e8e44b1e9b4c2a320de48227d0ce1daa
mayur1064 <mayurtrap@gmail.com>
2021-07-29 14:52:20 +0530

    data.sql

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
     password varchar(100) NOT NULL
 );
 
-INSERT INTO users (name, email, password ) VALUES   ( 'admin_dev', 'admin_dev@mail.com', 'SampleP@s123' );
-INSERT INTO users (name, email, password ) VALUES ( 'johny_dev', 'johny_dev@mail.com', 'SampleP@s123' );
-INSERT INTO users (name, email, password ) VALUES ( 'ramazan_dev', 'rmzn_dev@mail.com', 'SampleP@s123' );
-INSERT INTO users (name, email, password ) VALUES ( 'teten-nugraha_dev', 'teten_dev@mail.com', 'SampleP@s123' );
+INSERT INTO users (name, email, password ) VALUES   ( 'admin_dev1', 'admin_dev@mail.com', 'SampleP@s123' );
+INSERT INTO users (name, email, password ) VALUES ( 'johny_dev11', 'johny_dev@mail.com', 'SampleP@s123' );
+INSERT INTO users (name, email, password ) VALUES ( 'ramazan_dev111', 'rmzn_dev@mail.com', 'SampleP@s123' );
+INSERT INTO users (name, email, password ) VALUES ( 'teten-nugraha_dev111', 'teten_dev@mail.com', 'SampleP@s123' );
